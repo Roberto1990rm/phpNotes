8c4ca21dbd90af3f41cbcf38204c3abd1ed90a6b
roberto <robertoramirezmoreno@gmail.com>
2023-04-22 16:35:57 +0200

    phpNotes

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+//*Ciclos: if:
+//$varUno = 50;
+//$varDos = 5;
+//if ($varUno == $varDos)
+//echo "VarUno es igual que var dos\n";
+//else
+//echo "VarDos y VarUno son diferentes\n";//
+//*Ciclo: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
