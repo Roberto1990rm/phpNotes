57eee51941eba85a2f6fa32afaab46a8bfd83bab
roberto <robertoramirezmoreno@gmail.com>
2023-04-22 17:46:38 +0200

    if else, elseif, switch

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
 <?php
-
-//*Condición: if:
-//$varUno = 50;
-//$varDos = 5;
+$varUno = 50;
+$varDos = 5;
+//*Condición: "if"
 //if ($varUno == $varDos)
-//echo "VarUno es igual que var dos\n";
+//echo "VarUno es igual que var 
+//dos\n";
 //else
-//echo "VarDos y VarUno son diferentes\n";//
-//*Ciclo: 
+//echo "VarDos y VarUno son 
+//diferentes\n";//
+//*Condition: "if elseif elseif"
+//if ($varUno == 0){
+//    echo "cero";
+//}
+//elseif ($varUno == 1){
+//   echo "Uno";
+//}
+//elseif ($varUno >= 2){
+//   echo "all number is
+//welcome";
+//}
+//condition: "switch"
+//switch ($varUno) {
+    //case $varUno == 0:
+        //echo "cero\n";
+        //break;
+    //case $varUno == 1:
+        //echo "uno\n";
+        //break;
+    //case $varUno > 1:
+        //echo "all number is welcome";
+        //break;
+//}
+
+
+
+
 
 
 
