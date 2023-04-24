48182fccefa4f76308700164f6444222e9aca59f
roberto <robertoramirezmoreno@gmail.com>
2023-04-24 16:17:49 +0200

    foldes object

--- index2.php
+++ index2.php
@@ -1,5 +1,11 @@
 <?php
-//Introduccion programación de objetos.
+//* Introduccion programación orientada a objetos. 
+//** En resumen una propiedad es una variable o classe y un método es el equivalente a una function.
+//** El metodo puede variar si va dentro de una class.
+
+
+
+
 
 
 ?>
---
+++ objetos/clase1.php
@@ -0,0 +1,24 @@
+<?php
+
+$yo = new Person ();
+$yo->name = 'David';
+
+
+class PersonStudent {
+    public $name;  
+}
+
+echo $yo->name;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
